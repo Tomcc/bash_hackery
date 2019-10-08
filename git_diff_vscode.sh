0d08bc10325bbfc86a7309f6ef98bd33ebfabab0
Tommaso Checchi <tommaso.checchi1@gmail.com>
2019-10-07 22:44:32 -0700

    Fix

--- git_diff_vscode.sh
+++ git_diff_vscode.sh
@@ -6,7 +6,7 @@ temp_file=$(mktemp)
 mv "$temp_file" "$temp_file.diff"
 
 # show all changed files
-git diff &> "$temp_file.diff"
+git diff HEAD &> "$temp_file.diff"
 
 # show untracked files
 git ls-files --others --exclude-standard | xargs -n 1 git --no-pager diff /dev/null >> "$temp_file.diff"
