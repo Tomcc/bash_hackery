a4be636bffe7582b9fda4d8523fe310094b22420
Tommaso Checchi <tommaso.checchi1@gmail.com>
2019-10-07 22:40:17 -0700

    Added a weird thing to show the current diff in vscode

---
+++ git_diff_vscode.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# create a temp file
+temp_file=$(mktemp)
+
+mv "$temp_file" "$temp_file.diff"
+
+# show all changed files
+git diff &> "$temp_file.diff"
+
+# show untracked files
+git ls-files --others --exclude-standard | xargs -n 1 git --no-pager diff /dev/null >> "$temp_file.diff"
+
+code -n "$temp_file.diff"
