176192e4597b29f18711d90cbbb6e9ecbd567c00
Tommaso Checchi <tommaso.checchi1@gmail.com>
2022-02-21 19:24:30 -0800

    Use brew nvm which is a lot cleaner

--- nvm_hook.zsh
+++ nvm_hook.zsh
@@ -1,7 +1,7 @@
 
 # NVM config
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+export NVM_DIR="$HOME/.nvm"
+source "$(brew --prefix nvm)/nvm.sh"
 
 # place this after nvm initialization!
 autoload -U add-zsh-hook
