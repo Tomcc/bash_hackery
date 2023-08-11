c9b758fc34ffbbc0b8fd4aaf15c51e89af70776b
Tommaso Checchi <tommaso.checchi1@gmail.com>
2023-08-11 00:57:36 -0700

    A pathetic attempt at a script that sets up a mac

---
+++ setup_mac.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# TODO install brew
+
+brew install git install zsh atuin direnv alfred alt-tab iterm2 karabiner-elements secretive stats vlc
+
+# TODO install rust
+
+# TODO install nvm
