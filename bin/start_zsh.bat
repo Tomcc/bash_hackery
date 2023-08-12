eab13cc9b13b00398a5dc3ffacac820fcd45de8b
Tommaso Checchi <tommaso.checchi1@gmail.com>
2023-08-12 14:52:22 -0700

    Added a smol windows note

--- bin/start_zsh.bat
+++ bin/start_zsh.bat
@@ -4,4 +4,6 @@ setlocal
 set MSYS=winsymlinks:nativestrict
 set MSYS2_PATH_TYPE=inherit
 
+@REM To share "HOME" with windows, set `db_home: windows` in /etc/nsswitch.conf within msys2
+
 %USERPROFILE%/scoop\apps\msys2\current\msys2_shell.cmd -mingw64 -no-start -defterm -c zsh 
