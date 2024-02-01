d649a3687856a0b6aab94308b8143e3e21ced078
Tommaso Checchi <tommaso.checchi1@gmail.com>
2024-01-31 18:04:49 -0800

    Added git-mergemain.sh, a script to merge the current branch into main

---
+++ bin/git-mergemain.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Require a message in $1
+if [ -z "$1" ]; then
+  echo "Usage: git mergemain.sh <commit message>"
+  exit 1
+fi
+
+set -euo pipefail
+
+# make sure that there are no outstanding git changes
+if [[ -n $(git status --porcelain) ]]; then
+  echo "There are outstanding changes in the working directory. Please commit or stash them before running this script."
+  exit 1
+fi
+
+# make sure that the current branch is not the main branch
+if [[ $(git rev-parse --abbrev-ref HEAD) == "main" ]]; then
+  echo "You are currently on the main branch. Please switch to a different branch before running this script."
+  exit 1
+fi
+
+# make sure that the main branch is up to date
+git fetch origin main
+
+# forcibly reset local main to match origin/main
+# we do this to avoid checking out a potentially very old main just to update it
+git push . origin/main:main --force
+
+# save the current branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# switch to the main branch
+git switch main
+
+# merge the current branch into main with the message in $1
+git merge --no-ff -m "$1" $current_branch 
+
+# push the changes to the remote
+git push origin main
+
+# switch back to the original branch
+git switch -
